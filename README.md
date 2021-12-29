6039e36c40a6a39219b0476ac286cc1552dfa71f
emmacornio <emmacornio@protonmail.com>
2021-12-29 01:34:37 -0600

    Finish last details
    Added Rainbow mode as wll as an eraser mode
    also added some styles with css and slightly modified the index

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # etch-a-sketch
-etch-a-sketch project from The Odin Project
+
+etch-a-sketch project from [The Odin Project](https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/etch-a-sketch-project)
+
+check the [demo]()
+
+## Objective
+
+The objective of this project is to display my abilities modifiying the DOM
+with JavaScript
--- scrtip.js
+++ scrtip.js
@@ -5,12 +5,57 @@ const sketch = document.getElementById("sketch-grid");
 const setBtn = document.getElementById("set-btn");
 const grid = document.getElementById("sketch-grid");
 const clearBtn = document.getElementById("clear-btn");
+const classicBtn = document.getElementById("classic-btn");
+const rainbowBtn = document.getElementById("rainbow-btn");
+const eraserBtn = document.getElementById("fancy-btn");
+
 let gridElement = document.getElementsByClassName("grid-div");
-const DEFAULTGRID = 30;
+const DEFAULTGRID = 15;
 sizeSlider.value = DEFAULTGRID;
 const sketchStyle = window.getComputedStyle(sketch);
 
-window.addEventListener("load", createGrid(DEFAULTGRID));
+window.addEventListener("load", () => {
+	color.value = "#353535";
+	sliderValue.textContent = DEFAULTGRID;
+	createGrid(DEFAULTGRID);
+	classicBtn.classList.add("selected");
+	//classicBtn.classList.toggle("selected");
+	rainbowBtn.classList.add("selected");
+	rainbowBtn.classList.toggle("selected");
+	eraserBtn.classList.add("selected");
+	eraserBtn.classList.toggle("selected");
+});
+classicBtn.addEventListener("click", () => {
+	if (classicBtn.classList.contains("selected")) return;
+	else classicBtn.classList.toggle("selected");
+	if (eraserBtn.classList.contains("selected")) {
+		eraserBtn.classList.toggle("selected");
+	}
+	if (rainbowBtn.classList.contains("selected")) {
+		rainbowBtn.classList.toggle("selected");
+	}
+});
+rainbowBtn.addEventListener("click", () => {
+	if (rainbowBtn.classList.contains("selected")) return;
+	else rainbowBtn.classList.toggle("selected");
+	if (classicBtn.classList.contains("selected")) {
+		classicBtn.classList.toggle("selected");
+	}
+	if (eraserBtn.classList.contains("selected")) {
+		eraserBtn.classList.toggle("selected");
+	}
+});
+
+eraserBtn.addEventListener("click", () => {
+	if (eraserBtn.classList.contains("selected")) return;
+	else eraserBtn.classList.toggle("selected");
+	if (classicBtn.classList.contains("selected")) {
+		classicBtn.classList.toggle("selected");
+	}
+	if (rainbowBtn.classList.contains("selected")) {
+		rainbowBtn.classList.toggle("selected");
+	}
+});
 
 sizeSlider.addEventListener("input", () => {
 	//console.log(sizeSlider.value);
@@ -32,18 +77,20 @@ function createGrid(value) {
 		gridDiv.className = "grid-div";
 		gridDiv.classList.add("black");
 		gridDiv.classList.toggle("black");
-
-		// gridDiv.style.cssText = `height:${
-		// 	parseFloat(sketchStyle.height) / value
-		// }px ;width:${parseFloat(sketchStyle.width) / value}px`;
 		gridDiv.addEventListener("mouseover", (e) => changeColor(e.target));
 		grid.appendChild(gridDiv);
 	}
 	sketch.style.cssText = `grid-template-columns:repeat(${value}, 1fr);grid-template-rows:repeat(${value}, 1fr)`;
 }
 function changeColor(div) {
-	//div.classList.toggle("black", true);
-	div.style.cssText = `background-color:${color.value}`;
+	if (classicBtn.classList.contains("selected")) {
+		div.style.cssText = `background-color:${color.value}`;
+	} else if (rainbowBtn.classList.contains("selected")) {
+		let randomHue = Math.floor(Math.random() * 360);
+		div.style.cssText = `background-color: hsl(${randomHue}, 93%, 82%);`;
+	} else if (eraserBtn.classList.contains("selected")) {
+		div.style.cssText = `background-color=#dfe7fd;`;
+	}
 }
 
 function clear() {
